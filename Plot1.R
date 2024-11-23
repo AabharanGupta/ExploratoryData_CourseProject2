89d3c43fd8291f63e94e27a6d8177ba8cde20e4c
AabharanGupta <125910637+AabharanGupta@users.noreply.github.com>
2024-11-24 01:53:53 +0530

    code and data

---
+++ Plot1.R
@@ -0,0 +1,19 @@
+library(plyr)
+library(ggplot2)
+library(data.table)
+library(grid)
+library(scales)
+library(httr)
+
+if (!"NEI" %in% ls()) {
+  NEI <- readRDS("../exdata_data_NEI_data/summarySCC_PM25.rds")
+}
+if (!"SCC" %in% ls()) {
+  SCC <- readRDS("../exdata_data_NEI_data/Source_Classification_Code.rds")
+}
+png(filename = "./Plot1.png", width = 480, height = 480, units = "px")
+if (!"totalEmissions" %in% ls()) {
+  totalEmissions <- aggregate(NEI$Emissions, list(NEI$year), FUN = "sum")
+}
+plot(totalEmissions, type = "b", pch = 13, col = "red", xlab = "Year", main = expression('Total PM'[2.5]*" Emissions in the United States from 1999 to 2008"), ylab = expression('Total PM'[2.5]*" Emission (in tons)"))
+dev.off()
---
+++ Plot4.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(data.table)
+library(grid)
+library(scales)
+library(httr)
+
+if (!"NEI" %in% ls()) {
+  NEI <- readRDS("../exdata_data_NEI_data/summarySCC_PM25.rds")
+}
+if (!"SCC" %in% ls()) {
+  SCC <- readRDS("../exdata_data_NEI_data/Source_Classification_Code.rds")
+}
+if (!"CoalNEI" %in% ls()) {
+  Coal <- SCC[grep("Comb.*Coal", SCC$Short.Name), "SCC"]
+  CoalNEI <- NEI[NEI$SCC %in% Coal, ]
+}
+if (!"TData" %in% ls()) {
+  TData <- ddply(CoalNEI, .(year), summarise, TotalEmissions = sum(Emissions))
+}
+png(filename = "Plot4.png", width = 480, height = 480, units = "px")
+ggplot(TData, aes(year, TotalEmissions)) + geom_line(color = "red") + geom_point() + labs(title = "Total Emissions from Coal Combustion-Related Sources", x = "Year", y = "Total Emissions")
+dev.off()
